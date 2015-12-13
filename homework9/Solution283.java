ada48875bb5d087459170112f76bcf08ab41bbd3
zhangmao6237 <418512350@qq.com~>
2015-12-13 12:59:45 +0800

    homework9

---
+++ homework9/Solution283.java
@@ -0,0 +1,25 @@
+/*
+Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
+
+Note:
+You must do this in-place without making a copy of the array.
+Minimize the total number of operations.
+*/
+
+public class Solution283 {
+	public void moveZeroes(int[] nums) {
+		int len = nums.length;
+		//int[] flags = new int[len];
+		int flag = 0;
+		for (int i=0; i<len; i++) {
+			if (nums[i]!=0) {
+				nums[flag] = nums[i];
+				flag++;
+			}
+		}
+
+		for (int i=flag; i<len; i++) nums[i] = 0;
+	}
+}
